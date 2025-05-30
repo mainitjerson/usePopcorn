ca97a9dc04b236cec2a928dfc70760443cd28a43
mainitjerson <117341243+mainitjerson@users.noreply.github.com>
2025-05-30 16:38:40 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Box from "./components/Box";
